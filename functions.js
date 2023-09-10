9f225f714c4292d22afe0de4fed2011fd45d5252
Jeorel <zabjeo@gmail.com>
2023-09-10 18:20:36 +0800

    update commit

---
+++ functions.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+
+const functions ={
+    add:(num1, num2) => num1+num2,
+    isNull:()=>null,
+    checkValue:(x)=>x,
+    createUser:()=>{
+            const user = {email:'kodego@test.com', password:'123456789'}
+            return user;
+    },
+    fetchUser:()=>axios.get('https://jsonplaceholder.typicode.com/users/1')
+    .then(res=>res.data)
+    .catch(err=>'something went wrong!')
+};
+
+module.exports = functions;
